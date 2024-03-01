45c372025094bf4c3183ed1aceae05f021bffa2b
Dmitry Tolochko <DimaTol36@gmail.com>
2024-03-01 12:19:53 +0500

    фикс багов и деплой

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,9 +1,21 @@
 import { NavLink, useParams } from "react-router-dom";
 import arrow from '../../images/icons/right-arrow.svg';
 import './Menu.scss';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { api } from "../../App";
 
 export default function Menu() {
     const params = useParams();
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+        axios.get(api + 'api/Employee/' + params.id)
+        .then(response => {
+            setName(response.data.name);
+        });
+
+    }, [params])
 
     return (<>
         <div className="menu-wrapper">
@@ -13,7 +25,7 @@ export default function Menu() {
                 <NavLink to='/' className='menu-option'>Список сотрудников</NavLink>
                 {params?.id ? (<>
                     <img alt='arrow' src={arrow}/>
-                    <NavLink to='/profile/1' className='menu-option'>Дмитриев Игорь Степанович</NavLink>
+                    <NavLink to='/profile/1' className='menu-option'>{name}</NavLink>
                 </>) : <></>}
                
             </div>
